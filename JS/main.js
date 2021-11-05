14f153c5d06af04e22a44514b3e43e1741383e3e
emmanuelJM <68725141+emmanuelJM@users.noreply.github.com>
2021-11-05 14:21:19 -0600

    v16

--- JS/main.js
+++ JS/main.js
@@ -108,11 +108,3 @@ $(document).ready(function(){
 //AOS Animate
 
 AOS.init();
-
-// Global site tag (gtag.js) - Google Analytics 
-
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-
-gtag('config', 'G-RWC7Z0XSP2');
